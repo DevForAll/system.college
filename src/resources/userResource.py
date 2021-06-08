c4ea4fb5f356c2abbcebf276bb1cb5adab66ee24
Jhon Ochochoque Aguirre <jhon.ochochoque@alignet.com>
2021-06-07 23:40:19 -0500

    modificacion del metodo para obtener la lista de usuarios

--- src/resources/userResource.py
+++ src/resources/userResource.py
@@ -1,8 +1,12 @@
 from flask_restful import Resource
 from flask import request
+from models.userModel import UserModels
+from schema.userSchema import UserSchema
 
+
+user_list_schema = UserSchema(many=True)
 
 class User(Resource):
     @classmethod
     def get(cls):
-        return {"message":"Hello world, Usuarios"}, 200
+        return {"users": user_list_schema.dump(UserModels.find_all_users())}, 200
