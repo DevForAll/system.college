3689e97c06d704ac18449c84363c08df002e6346
Jhon Ochochoque Aguirre <jhon.ochochoque@alignet.com>
2021-06-08 00:08:38 -0500

    modificacion para configuracion de la url de POSTGRES, obtencion de la instancia db y ma

--- src/app.py
+++ src/app.py
@@ -1,16 +1,27 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from resources.userResource import User
+from db import db
+from dotenv import load_dotenv
+from ma import ma
+
 
 #   CREACION DE APP 
 app = Flask(__name__)
+# load_dotenv(".env")
+app.config['DEBUG'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:admin@localhost/school"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 api = Api(app)
-
 #   AGREGANDO ENDPOINTS
 api.add_resource(User, "/users")
 
 
 if __name__ == '__main__':
     #   EJECUCION DE NUESTRA APP
+    db.init_app(app)
+    ma.init_app(app)
     app.run(port=5000, debug=True)
